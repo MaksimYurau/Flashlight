16c4b62652f132da55b7217d40b3ce0d3383bb46
MaksimYurau <2004softwaredeveloper@gmail.com>
2022-01-23 19:08:36 +0300

    Initial commit

---
+++ app/src/main/java/com/maksimyurau/android/flashlight/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.maksimyurau.android.flashlight
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
